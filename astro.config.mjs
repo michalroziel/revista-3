2cac067e4e287203ed65c39f72b202cb385704ae
Erfi Anugrah <erfi.anugrah@gmail.com>
2025-08-27 13:29:53 +0200

    v5.7.7 -- fix navigation part 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,9 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.GITHUB_PAGES === 'true' ? "https://erfianugrah.github.io" : "https://www.erfianugrah.com",
-  base: process.env.GITHUB_PAGES === 'true' ? "/revista-3" : undefined,
+  site: process.env.GITHUB_PAGES === "true"
+    ? "https://erfianugrah.github.io/revista-3"
+    : "https://www.erfianugrah.com",
 
   image: {
     // responsiveStyles: true,
